7d6cd65b5b838a6b1b015572845b0d5efd6e6791
zqnet <68742966+zqnet@users.noreply.github.com>
2020-10-17 09:50:50 +0800

    mac vscode maven springboot
    springboot demo1
    maven config
    macos

---
+++ 2020-10-17vscode-springboot/demo/src/main/java/com/xzhang/demo/TestController.java
@@ -0,0 +1,13 @@
+package com.xzhang.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("test")
+public class TestController {
+    @RequestMapping("")
+    public String test(){
+        return "testController test";
+    }
+}
